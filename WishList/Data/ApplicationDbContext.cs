c2201d76b97c53c1e852fc013083468e3dd49ba2
Ananyahaha <ananyam1999@gmail.com>
2021-09-22 23:14:26 +0530

    Completed module

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Item>      
+    }
+}
---
+++ WishList/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using  System.ComponentModel.DataAnnotations;
+using WishList.Data;
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+        [Required, MaxLength(50)]
+        public string Description;
+    }
+}
+
